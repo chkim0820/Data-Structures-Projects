0582a30fb844027bbe49bed9d7ec0154af551500
CHK0820 <109783563+chkim0820@users.noreply.github.com>
2022-09-19 17:48:08 -0400

    1st draft of the methods in part 2

--- HW1/Fibonacci.java
+++ HW1/Fibonacci.java
@@ -5,6 +5,17 @@
 
 public class Fibonacci {
 
+    private int[] fibArray;
+    private int size;
+
+    /**
+     * A constructor for the Fibonacci class that initializes the array with an appropriate number
+     */
+    public void fibonacci(int num) {
+        fibArray = new int[num + 10]; // initializing the array at a size 10 bigger than given
+        size = 0;
+    }
+
     /**
      * a method that prints out the nth Fibonacci sequence starting from 0, 1, using an iterative method
      * @param n specifies the nth sequence
@@ -41,13 +52,30 @@ public class Fibonacci {
      * The efficiency of the two methods in terms of time complexity & big-o notation as comments
      */
 
-
+     /**
+      * A helper method that finds the appropriate index of the input number using binary search
+      * @param num the number that needs an appropriate index assigned
+      * @return the appropriate index for the input number
+      */
+    public int indexFinder (int num) { // return 4 to denote when there's no value
+        int ptr = (size - 1) / 2;
+        while ((ptr != 0) || (ptr != size)) {
+            if (fibArray(ptr) == num
+                return ptr;
+            else if (fibArray(ptr) < num)
+                ptr = ptr + ptr/2;
+            else if (fibArray(ptr) > num)
+                ptr = ptr - ptr/2;
+        }
+        return 4;
+    }
     /**
      * add a number to the list (allow to add duplicates)
      * @param item an item to be added to the list
      */
     public void add(int item) {
-
+        int temp = indexFinder(item);
+        while () // while loop to 
     }
 
     /**
@@ -55,14 +83,19 @@ public class Fibonacci {
      * @param item an item to be removed from the list
      */
     public void remove(int item) {
-
+        int temp = indexFinder(item);
+        while (temp < size) {
+            fibArray[temp] = fibArray[temp + 1];
+        }
     }
 
     /**
      * check if the item exists in the list
      */
     public boolean ifContains(int item) {
-
+        if (indexFinder(item) == 4)
+            return false;
+        else return true;
     }
 
     /**
@@ -70,14 +103,17 @@ public class Fibonacci {
      * @return a random number from the list
      */
     public int grab() {
-
+        return indexFinder((java.lang.Math.random()) * size);
     }
 
     /**
      * prints out the number of unique items in the list
      */
     public int numItems() {
-
+        if (size == 1)
+            return 1;
+        else
+            return size - 1;
     }
 
     /**
