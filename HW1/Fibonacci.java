26b10ddb1cf93621ba822e1e8592255b48481c21
CHK0820 <109783563+chkim0820@users.noreply.github.com>
2022-09-20 10:08:53 -0400

    added comments and finished the JUnit document

--- HW1/Fibonacci.java
+++ HW1/Fibonacci.java
@@ -5,15 +5,18 @@
 
 public class Fibonacci {
 
+    /** an int array for the Fibonacci object */
     private int[] fibArray;
+    /** the size of the array (number of un-unique items) */
     private int size;
+    /** number of index in the array initialized */
     private int numIndex;
 
     /**
      * A constructor for the Fibonacci class that initializes the array with an appropriate number
      */
-    public void fibonacci(int num) {
-        numIndex = num + 10;
+    public Fibonacci(int num) {
+        numIndex = num + 10; // number of index for the array (10 more for safety)
         fibArray = new int[numIndex]; // initializing the array at a size 10 bigger than given
         size = 0;
     }
@@ -21,9 +24,9 @@ public class Fibonacci {
     /**
      * A constructor for the Fibonacci class that doesn't get an input
      */
-    public void fibonacci() {
-        numIndex = 0;
-        fibArray = new int[0];
+    public Fibonacci() {
+        fibArray = new int[30];
+        numIndex = 30;
         size = 0;
     }
 
@@ -33,12 +36,14 @@ public class Fibonacci {
      * @return returns the nth sequence
      */
     public int fibonacciIterative(int n) {
-        int first = 1;
-        int second = 2;
-        if (n == 0)
+        int first = 1; // first of the two consecutive Fibonacci numbers
+        int second = 2; // second of the two consecutive Fibonacci numbers
+        // edge cases; the first three of the sequence
+        if (n == 0) 
             return 0;
         else if (n == 1 || n == 2)
             return 1;
+        // going through Fibonacci sequence to find the appropriate number
         for (int i = 0; i < n - 3 ; i++) {
             int temp = first;
             first = second;
@@ -54,7 +59,7 @@ public class Fibonacci {
      * @return returns the nth sequence
      */
     public int fibonacciRecursive (int n) {
-        if (n <= 1)
+        if (n <= 1) // base case
             return 1;
         return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
     }
@@ -65,9 +70,9 @@ public class Fibonacci {
       * @param num the number that needs an appropriate index assigned
       * @return the appropriate index for the input number
       */
-    public int indexFinder (int num) { // return 4 to denote when there's no value
-        int front = 0;
-        int back = size - 1;
+    public int indexFinder (int num) {
+        int front = 0; // front of array
+        int back = size - 1; // back of array
         while (front <= back) {
             int half = (back + front) / 2;
             if (fibArray[half] == num)
@@ -83,16 +88,26 @@ public class Fibonacci {
      * add a number to the list (allow to add duplicates)
      * @param item an item to be added to the list
      */
-    public void add(int item) throws NullPointerException {
-        int index = Math.abs(indexFinder(item));
+    public void add(int item) {
+        int index = Math.abs(indexFinder(item)); // appropriate index for the new item
+        // enough room in the array for new items
         if (size <= numIndex) {
             while (index < size) {
                 fibArray[index] = fibArray[index + 1];
             }
-            size++;
         }
-        else
-            throw new NullPointerException();
+        // not enough room in the array; create an array that is big enough
+        else {
+            int[] temp = new int[numIndex + 1];
+            for (int i = 0; i < index; i++)
+                temp[i] = fibArray[i];
+            temp[index] = item;
+            for (int i = index + 1; i < size) {
+                temp[i] = fibArray[i + 1];
+            fibArray = temp;
+            }
+        }
+        size++;
     }
 
     /**
@@ -100,7 +115,7 @@ public class Fibonacci {
      * @param item an item to be removed from the list
      */
     public void remove(int item) {
-        int index = indexFinder(item);
+        int index = indexFinder(item); // appropriate index for the item we're looking for
         if (index >= 0) {
             while (index < size - 1) {
                 fibArray[index] = fibArray[index + 1];
@@ -130,10 +145,22 @@ public class Fibonacci {
      * prints out the number of unique items in the list
      */
     public int numItems() {
-        if (size <= 2)
-            return size;
-        else
-            return size - 1;
+        int noDupl = 0; // number of non-duplicative items in the array
+        int last = fibArray[0]; // last array checked
+        for (int i = 0; i < size; i++, last = fibArray[i]) {
+            if (last != fibArray[i])
+                noDupl++;
+        }
+        return noDupl;
+    }
+
+    /**
+     * a helper method that returns the value that is stored in the array
+     * @param index index of the value 
+     * @return the value at the given index
+     */
+    public int lookup(int index) {
+        return fibArray[index];
     }
 
     /**
@@ -144,7 +171,7 @@ public class Fibonacci {
         Fibonacci subject = new Fibonacci();
         for (int i = 0; args[i] != null; i++) {
             int index = Integer.parseInt(args[i]);
-            System.out.println(subject.fibonacciIterative(index));
+            subject.add(index);
         }
     }
 }
--- HW1/FibonacciTest.java
+++ HW1/FibonacciTest.java
@@ -1,8 +1,121 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 /**
  * JUnit test class for Fibonacci class
  * @author Chaehyeon Kim cxk445
  */
-public class FibonacciTest {
+public class FibonacciTest{
     
-    public testFibonacciRecursive
+    /** an object of Fibnacci class for testing */
+    Fibonacci fib = new Fibonacci();
+
+    /**
+     * test method for fibonacciIterative() in Fibonacci class
+     */
+    public void testFibonacciIterative() {
+        assertEquals("iterative input 0", fib.fibonacciIterative(0), 0);
+        assertEquals("iterative input 1", fib.fibonacciIterative(1), 1);
+        assertEquals("iterative input 2", fib.fibonacciIterative(2), 1);
+        assertEquals("iterative input 8", fib.fibonacciIterative(8), 13);
+    }
+
+    /**
+     * test method for fibonacciRecursive() in Fibonacci class
+     */
+    public void testFibonacciRecursive() {
+        assertEquals("iterative input 0", fib.fibonacciRecursive(0), 0);
+        assertEquals("iterative input 1", fib.fibonacciRecursive(1), 1);
+        assertEquals("iterative input 2", fib.fibonacciRecursive(2), 1);
+        assertEquals("iterative input 8", fib.fibonacciRecursive(8), 13);
+    }
+
+    /**
+     * test method for add() in Fibonacci class
+     */
+    public void testAdd() {
+        for (int i = 0; i < 10; i++) { // adds 10 fibonacci numbers to the array
+            fib.add(fib.fibonacciIterative(i));
+        }
+
+        assertEquals("add input fib index 0", fib.lookup(0), 0);
+        assertEquals("add input fib index 1", fib.lookup(1), 1);
+        assertEquals("add input fib index 2", fib.lookup(2), 1);
+        assertEquals("add input fib index 3", fib.lookup(3), 2);
+        assertEquals("add input fib index 4", fib.lookup(4), 3);
+        assertEquals("add input fib index 5", fib.lookup(5), 5);
+        assertEquals("add input fib index 6", fib.lookup(6), 8);
+        assertEquals("add input fib index 7", fib.lookup(7), 13);
+        assertEquals("add input fib index 8", fib.lookup(8), 21);
+        assertEquals("add input fib index 9", fib.lookup(9), 34);
+        
+        fib.add(-1); // adding random numbers to the array to test inserting in front, middle, end
+        fib.add(4);
+        fib.add(35);
+        assertEquals("add input -1", fib.lookup(0), -1);
+        assertEquals("add input 4", fib.lookup(7), 4);
+        assertEquals("add input 35", fib.lookup(13), 35);
+    }
+
+    /**
+     * test method for remove() in Fibonacci class
+     */
+    public void testRemove() {
+        fib.remove(-1); // removing the random numbers added to the array in the previous method
+        fib.remove(4);
+        fib.remove(35);
+
+        assertEquals("remove input -1", fib.ifContains(-1), false);
+        assertEquals("remove input 4", fib.ifContains(4), false);
+        assertEquals("remove input 35", fib.ifContains(35), false);
+    }
+
+    /**
+     * test method for ifContains() in Fibonacci class
+     */
+    public void testIfContains() {
+        assertEquals("ifContains(0) returns false", fib.ifContains(0), true);
+        assertEquals("ifContains(5) returns false", fib.ifContains(5), true);
+        assertEquals("ifContains(34) returns false", fib.ifContains(34), true);
+        assertEquals("ifContains(-2) returns true", fib.ifContains(-2), false);
+        assertEquals("ifContains(6) returns true", fib.ifContains(6), false);
+        assertEquals("ifContains(14) returns true", fib.ifContains(14), false);
+    }
+
+    public void testGrab() {
+        assertEquals(fib.ifContains(fib.grab()), true);
+        assertEquals(fib.ifContains(fib.grab()), true);
+        assertEquals(fib.ifContains(fib.grab()), true);
+        assertEquals(fib.ifContains(fib.grab()), true);
+        assertEquals(fib.ifContains(fib.grab()), true);
+        // testing multiple times to ensure the method really works
+    }
+
+    public void testNumItems() {
+        assertEquals("numItems() for 10 items", fib.numItems(), 10);
+
+        Fibonacci fib2 = new Fibonacci(); // emtpy Fibonacci object
+        assertEquals("numItems() for an empty array",fib.numItems(), 0);
+
+        fib2.add(1); // one item
+        assertEquals("numItems() for an array with one item", fib.numItems(), 1);
+
+        fib2.add(1); // many duplicates; still one item
+        fib2.add(1);
+        fib2.add(1);
+        assertEquals("numItems() for an array with duplicates", fib.numItems(), 1);
+
+        fib2.add(2); // two items
+        assertEquals("numItems() with two distinct items", fib.numItems(), 2);
+
+        fib2.add(1); // two items; it's a duplicate
+        assertEquals("numItems() adding another duplicate", fib.numItems(), 2);
+    }
+
+    /**
+     * test the main method in Fibonacci class
+     */
+    public void testMainMethod() {
+
+    }
 }
