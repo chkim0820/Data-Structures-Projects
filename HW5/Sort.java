46bbfdf1f67f398df357893dbbf7918f4a93c9e9
CHK0820 <109783563+chkim0820@users.noreply.github.com>
2022-11-24 18:51:18 -0500

    Added JavaDoc comments to all the methods

---
+++ HW5/Sort.java
@@ -0,0 +1,73 @@
+/**
+ * Sort class for CSDS 233 HW 5
+ * @author Chaehyeon Kim cxk445
+ */
+public class Sort {
+
+    /**
+     * Uses the insertion sort algorithm to sort the input array in descending order.
+     * @param arr an input array of integers.
+     */
+    public static void insertionSort(int[] arr) {
+
+    }
+
+    /**
+     * Uses the bubble sort algorithm to sort the input array in descending order.
+     * @param arr an input array of integers.
+     */
+    public static void bubbleSort(int[] arr) {
+
+    }
+
+    /**
+     * Uses the shell sort algorithm using Hibbard’s sequence to sort the input array in descending order.
+     * @param arr an input array of integers.
+     */
+    public static void shellSort(int[] arr) {
+
+    }
+
+    /**
+     * Uses the quick sort algorithm to sort the input array in descending order.
+     * @param arr an input array of integers.
+     */
+    public static void quickSort(int[] arr) {
+
+    }
+
+    /**
+     * Uses the merge sort algorithm to sort the input array into descending order.
+     * @param arr an input array of integers.
+     */
+    public static void mergeSort(int[] arr) {
+
+    }
+
+    /**
+     * Uses the upgraded quick sort version from written problem 2 and 3 to sort the input array into descending order, where we switch to merge sort when it reaches the depth limit d and switch to insertion sort when the subarrays have fewer than k elements.
+     * @param input an input array of integers.
+     * @param d the depth limit.
+     * @param k the minimum number of elements a subarray can have before switching to insertion sort.
+     */
+    public static void upgradedQuickSort(int[] input, int d, int k) {
+
+    }
+
+    /**
+     * Returns an array of random integers of size n.
+     * @param n the size of an array to be returned.
+     * @return an array of random integers of size n.
+     */
+    public static int[] generateRandomArray(int n) {
+        return null;
+    }
+
+    /**
+     * Reads the commands specified in the file and then prints the corresponding result of the commands.
+     * @param filepath a String input corresponding to the filepath of a .txt file.
+     */
+    public static void readCommands(String filepath) {
+
+    }
+}
